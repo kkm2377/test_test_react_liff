d0ed867a6b603e84c2073adb5bdaed66d23fe6ab
epaew <epaew.333@gmail.com>
2022-05-04 20:02:08 +0900

    tmp

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -2,8 +2,8 @@ import { default as liff } from '@line/liff';
 import { render, waitFor } from '@testing-library/react';
 import { FC } from 'react';
 
-import { LiffProvider, useLiff } from '#/index';
-import { Liff } from '#/types';
+import { LiffProvider, useLiff } from './index';
+import { Liff } from './types';
 
 jest.mock('@line/liff');
 
